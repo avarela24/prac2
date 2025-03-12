1fea7ef14ac62a7602f61169fc9dc2fae4bd0d1f
avarela24 <alejandrovarelamartin@uma.es>
2025-03-12 14:14:20 +0100

    Updated class Date. Modification of the constructor

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -16,6 +16,7 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
+  private int fieldAddedByCollaborator;
   private final int day;
   private final int month;
   private final int year;
@@ -23,6 +24,8 @@ public class Date {
   public void methodAddedByOwner(){
     System.out.println("Method added by the owner");
   }
+
+
 
   /**
    * Retrieves the day component of this date.
@@ -59,10 +62,12 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  
+  // Constructor modified by the owner
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
   }
 
   /**
